fcb410f526b10179ef9ed1e0ac4e0dd021fb6dd7
Manuel254 <immanuelkituku@gmail.com>
2022-11-30 03:28:21 -0800

    Fix 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -10,6 +10,6 @@ for number in range(0, 100):
     # and puts 0 to 10 into 2 digits
     # then adds a comma and space
     if number != 99:
-        print(f"{number:02d}", end=", ")
+        print("{:02d}".format(number), end=", ")
     else:
-        print("{}".format(number))
+        print("{:02d}".format(number))
