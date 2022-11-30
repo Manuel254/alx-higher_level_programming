50f8b188bdaf221616f195b7e978a798166990c9
Manuel254 <immanuelkituku@gmail.com>
2022-11-30 10:15:42 -0800

    Add python script that converts a string to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""A program that prints a
+   string in uppercase
+"""
+
+
+def uppercase(str):
+    """Returns a string in uppercase"""
+    new_str = ''
+
+    for character in str:
+        # Checks whether character is alphabet
+        if character.isalpha():
+            # Converts character to unicode if alphabet
+            unicode_value = ord(character)
+
+            # Checks whether character is lowercase
+            # and converts it to uppercase
+            if unicode_value >= 97 and unicode_value <= 122:
+                unicode_value -= 32
+
+            # Converts unicode to character
+            character = chr(unicode_value)
+        new_str += character
+
+    print(new_str)
