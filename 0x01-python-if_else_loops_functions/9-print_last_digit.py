ca36203faf72d1870a466535bead95a1723e2a3a
Manuel254 <immanuelkituku@gmail.com>
2022-11-30 12:01:36 -0800

    Add python script that prints last digit of a number

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""A program that prints the last digit"""
+
+
+def print_last_digit(number):
+    last_digit = abs(number) % 10
+
+    print("{:d}".format(last_digit), end="")
+    return last_digit
