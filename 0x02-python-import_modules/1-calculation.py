0b6cb728327d15dd91fdb51a0be0f0cc4155512a
Manuel254 <immanuelkituku@gmail.com>
2022-12-01 02:38:25 -0800

    Fix 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-
-a = 10
-b = 5
-
 if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
     # Prints add of a and b
     print("{} + {} = {}".format(a, b, add(a, b)))
 
