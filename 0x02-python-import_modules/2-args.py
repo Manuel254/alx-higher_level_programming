a5cf8d1d6650feef3d1d0f5ed23b6f3da78454c1
Manuel254 <immanuelkituku@gmail.com>
2022-12-01 11:25:34 -0800

    Add python script that prints command line arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    import sys
+
+    # Finds length of argv excluding file name
+    length = len(sys.argv) - 1
+
+    if length == 0:
+        # Handles zero arguments
+        print("{} arguments.".format(length))
+    elif length == 1:
+        # Handles one argument
+        print("{} argument:".format(length))
+    else:
+        # Handles more than one argument
+        print("{} arguments:".format(length))
+
+    for index in range(1, length + 1):
+	# Prints the arguments with their respective indexes
+        print("{}: {}".format(index, sys.argv[index]))
