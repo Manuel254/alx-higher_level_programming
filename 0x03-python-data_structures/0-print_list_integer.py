81033af5d069d37b1c47c73781a89274b1d1bd4b
Manuel254 <immanuelkituku@gmail.com>
2022-12-04 02:43:30 -0800

    Add python script that prints integers

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def print_list_integer(my_list=[]):
+    """Prints integers from list"""
+    for number in my_list:
+        print("{:d}".format(number))
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+This is a directory that contains all the tasks related to the project Python - Data Structures: Lists, Tuples
