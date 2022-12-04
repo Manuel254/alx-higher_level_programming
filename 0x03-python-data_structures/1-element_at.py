d909e52e1bb2e7ce025505d9ad56b55d9f4c0960
Manuel254 <immanuelkituku@gmail.com>
2022-12-04 02:52:31 -0800

    Add python script that finds element at specific index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+def element_at(my_list, idx):
+    """Finds element specified at a specific index"""
+    if idx < 0:
+        return
+    elif idx > len(my_list):
+        return
+    else:
+        return my_list[idx]
