a4df45890053adddb14e6f240834b722fb5ba90b
Manuel254 <immanuelkituku@gmail.com>
2022-12-04 03:01:09 -0800

    Fix 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     """Finds element specified at a specific index"""
-    if idx < 0:
-        return
-    elif idx > len(my_list):
-        return
-    else:
+    if idx >= 0 and idx <= len(my_list):
         return my_list[idx]
