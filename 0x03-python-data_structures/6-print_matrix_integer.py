b118100847ec29237e4d0b32d322a957f198f973
Manuel254 <immanuelkituku@gmail.com>
2022-12-04 23:24:26 -0800

    Add 6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    """Prints elements in matrix"""
+    for index1, row in enumerate(matrix):
+        for index2, column in enumerate(row):
+            if index2 != len(row) - 1:
+                print("{:d}".format(column), end=" ")
+            else:
+                print("{:d}".format(column))
