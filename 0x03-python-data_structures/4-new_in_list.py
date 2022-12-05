2aba3d675c4bf43a580109920ee47b773b29e337
Manuel254 <immanuelkituku@gmail.com>
2022-12-04 22:37:28 -0800

    Add python script that replaces an element from a copied list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    """Replaces an element in a copied list"""
+    new_list = my_list[:]
+    
+    for index, el in enumerate(new_list):
+        if index == idx:
+            new_list[idx] = element
+    return new_list
