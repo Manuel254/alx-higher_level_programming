b7bea5aa3829874323b03d345774779492037cc6
Manuel254 <immanuelkituku@gmail.com>
2022-12-07 13:24:50 -0800

    Final commit

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    """Prints a sorted dictionary by keys"""
+    new_dict = {}
+
+    keys = sorted(a_dictionary.keys())
+    new_dict = {k : a_dictionary[k] for k in keys}
+
+    for k, v in new_dict.items():
+        print("{}: {}".format(k, v))
