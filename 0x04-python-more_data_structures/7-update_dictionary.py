9978b8b6941686b3582647e0c4a5d90110a3500b
Manuel254 <immanuelkituku@gmail.com>
2022-12-09 02:20:01 -0800

    Add 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-	"""Adds key/value to dictionary or replace value"""
-	a_dictionary[key] = value
-	return a_dictionary
+    """Adds key/value to dictionary or replace value"""
+    a_dictionary[key] = value
+
+    return a_dictionary
