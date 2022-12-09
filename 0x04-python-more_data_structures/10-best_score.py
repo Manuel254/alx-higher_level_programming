49ad626e941d67d4c1810f48eff336141a23d855
Manuel254 <immanuelkituku@gmail.com>
2022-12-09 04:00:40 -0800

    Fix 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,9 +4,9 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return
 
-    keys = sorted(list(a_dictionary.keys()))
-    values = sorted(list(a_dictionary.values()))
+    keys = a_dictionary.keys()
+    values = a_dictionary.values()
 
-    position = values.index(values[-1])
+    max_score = max(zip(keys, values))
 
-    return keys[position]
+    return max_score[0]
