872b3f1de661fcd2239ed25e4b924c5af93d3d8f
Manuel254 <immanuelkituku@gmail.com>
2023-01-20 00:31:00 -0800

    Completed

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -58,7 +58,7 @@ class Square(Rectangle):
         return {
                 "id": self.id,
                 "x": self.x,
-                "size" : self.size,
+                "size": self.size,
                 "y": self.y
                 }
 
