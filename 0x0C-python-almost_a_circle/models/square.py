a220bee67af9a61f9a1d28dd72b2171d850c1469
Manuel254 <immanuelkituku@gmail.com>
2023-01-19 23:21:03 -0800

    Completed

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,8 +19,19 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Getter method for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter method for size"""
+        self.width = value
+        self.height = value
+
     def __str__(self):
-        """Weell-formatted square instance"""
+        """Well-formatted square instance"""
         return "[{}] ({}) {}/{} - {}".format(
                                             self.__class__.__name__,
                                             self.id,
