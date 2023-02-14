9352e1dcb850d202d2ddedd2ff9cf00f29cdcce4
Manuel254 <immanuelkituku@gmail.com>
2023-02-14 09:36:08 -0800

    Completed

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,12 +5,10 @@ COLLATE utf8mb4_unicode_ci;
 -- Convert first_table to UTF8
 USE hbtn_0c_0;
 ALTER TABLE first_table
-CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 -- Convert name field to UTF8
 USE hbtn_0c_0;
 ALTER TABLE first_table
 MODIFY name
 VARCHAR(256)
-CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
