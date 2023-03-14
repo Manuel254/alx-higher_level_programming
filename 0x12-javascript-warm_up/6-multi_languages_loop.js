e562d29d6465e93edd7352addf916d4405217c05
Manuel254 <immanuelkituku@gmail.com>
2023-03-14 11:25:24 -0700

    Completed

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const arr_str = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const arrStr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (const i of arr_str) {
+for (const i of arrStr) {
   console.log(i);
 }
