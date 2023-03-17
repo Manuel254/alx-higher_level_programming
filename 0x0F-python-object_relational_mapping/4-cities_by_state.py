541c5f61786a4a92751aa784fab2cc1a4827627b
Manuel254 <immanuelkituku@gmail.com>
2023-03-17 03:42:25 -0700

    Completed

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,10 +15,10 @@ if __name__ == '__main__':
     cur = db.cursor()
 
     cur.execute("SELECT cities.id, cities.name, states.name\
-		FROM cities\
-		INNER JOIN states\
-		ON cities.state_id = states.id\
-		ORDER BY cities.id")
+                FROM cities\
+                INNER JOIN states\
+                ON cities.state_id = states.id\
+                ORDER BY cities.id")
 
     rows = cur.fetchall()
 
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,11 +15,11 @@ if __name__ == '__main__':
     cur = db.cursor()
 
     cur.execute("SELECT cities.name\
-		FROM cities\
-		INNER JOIN states\
-		ON cities.state_id = states.id\
-        WHERE states.name = %s\
-		ORDER BY cities.id", (sys.argv[4], ))
+                FROM cities\
+                INNER JOIN states\
+                ON cities.state_id = states.id\
+                WHERE states.name = %s\
+                ORDER BY cities.id", (sys.argv[4], ))
 
     rows = cur.fetchall()
 
