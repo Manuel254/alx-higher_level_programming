9371395eb3ec2c5c5a4f4aca827d31d0934e955a
Manuel254 <immanuelkituku@gmail.com>
2023-03-18 12:50:38 -0700

    Create a model state class and a SQL database for the states table

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""This module creates a class definition  of a State"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """This class creates a template for the State table"""
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
