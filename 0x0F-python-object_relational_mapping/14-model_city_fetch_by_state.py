1e8a9efed988ed4ca365cec8742a5f2d88323056
Manuel254 <immanuelkituku@gmail.com>
2023-03-18 15:00:37 -0700

    Refactor query to fetch cities by state and print results in alphabetical order in 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -17,8 +17,8 @@ if __name__ == '__main__':
     session = Session()
     Base.metadata.create_all(engine)
 
-    states = session.query(State, City).filter(State.id == City.state_id)\
-            .order_by(City.id)
+    states = session.query(State, City).filter(
+            State.id == City.state_id).order_by(City.id)
 
     for state, city in states:
         print(f"{state.name}: ({city.id}) {city.name}")
