d67f923a1937656ba2e3daa35b67daf7ffb01906
Manuel254 <immanuelkituku@gmail.com>
2023-03-22 01:34:51 -0700

    Refactor relationship_city.py to remove unnecessary import statement

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -3,7 +3,6 @@
 """This module creates a class definition  of a City"""
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from relationship_state import Base, State
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
