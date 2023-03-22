7e720baf107ab5fcca80b94cebc68278211a7c2e
Manuel254 <immanuelkituku@gmail.com>
2023-03-22 01:11:32 -0700

    Add new files and classes for creating a relationship between the State and City tables. Also add a .gitignore file to ignore Python cache files

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+""" This is a script that creates the State 'Carlifornia' with
+the City 'San Francisco' from the database hbtn_0e_100_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from relationship_state import State, Base
+from relationship_city import City
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'\
+            .format(sys.argv[1], sys.argv[2], sys.argv[3], pool_pre_ping=True))
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = State(name="California")
+    city = City(name="San Francisco")
+    state.cities.append(city)
+    session.add(state)
+    session.commit()
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""This module creates a class definition  of a City"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_state import Base
+
+
+class City(Base):
+    """This class creates a template for the City table"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
