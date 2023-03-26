6fb3fd4cdefdad0cf8313053f968ceb94e84e78a
Manuel254 <immanuelkituku@gmail.com>
2023-03-26 05:23:33 -0700

    Refactor "callMeMoby" function to use let instead of implicit global variable

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const callMeMoby = function (x, theFunction) {
-  for (i = 0; i < x; i++) { theFunction(); }
+  for (let i = 0; i < x; i++) { theFunction(); }
 };
 
 module.exports = { callMeMoby };
