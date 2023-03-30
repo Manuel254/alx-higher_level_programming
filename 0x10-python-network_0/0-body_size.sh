7451b21809933b36f4d244a56c5592fca52fba9e
Manuel254 <Immanuelkituku@gmail.com>
2023-03-30 04:36:05 -0700

    FIx requirements issue

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script takes in a url and sends a request to that url
-# and displays the size of the body of the response
+# Takes in a url and sends a request to that url and displays the size of the body of the response
 curl -s -w '%{size_download}\n' -o /dev/null "$1"
