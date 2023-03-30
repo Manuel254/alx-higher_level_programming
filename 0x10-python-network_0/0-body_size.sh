e72de6c3de9db602661e70ce61cd01e1424e79aa
Manuel254 <Immanuelkituku@gmail.com>
2023-03-30 04:32:58 -0700

    Fix output of the response body to stdout

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script takes in a url and sends a request to that url
 # and displays the size of the body of the response
-curl -s -w %size_upload $1
+curl -s -w '%{size_download}\n' -o /dev/null $1
