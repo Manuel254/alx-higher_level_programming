0a82913124698551d876b6ab323f24b9f6d60fb9
Manuel254 <immanuelkituku@gmail.com>
2023-03-31 07:34:24 -0700

    Add script to retrieve and display GitHub commits using API

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Takes your github credentials and uses the github
+API to display your id
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    repo = argv[1]
+    owner = argv[2]
+
+    res = requests.get(f"https://api.github.com/repos/{owner}/{repo}/commits")
+    res = res.json()
+
+    for commit in res:
+        id = commit.get('sha')
+        author = commit.get('commit').get('author').get('name')
+        print(f"{id}: {author}")
