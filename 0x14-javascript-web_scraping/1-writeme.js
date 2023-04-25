23a7c108ac5e8102bc9111cbc4c436ea21a23d80
Manuel254 <immanuelkituku@gmail.com>
2023-04-25 10:40:12 +0000

    Add script that writes data to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const f = require('fs');
+const filename = process.argv[2];
+const data = process.argv[3]
+
+f.writeFile(filename, data, 'utf-8', function (err) {
+  if (err) console.error(err);
+});
