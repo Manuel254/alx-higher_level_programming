cc2c9c3bbd1fde1bd9b0707911bf27d9961e911b
Manuel254 <immanuelkituku@gmail.com>
2023-04-25 10:41:23 +0000

    Fix semistandard error

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 
 const f = require('fs');
 const filename = process.argv[2];
-const data = process.argv[3]
+const data = process.argv[3];
 
 f.writeFile(filename, data, 'utf-8', function (err) {
   if (err) console.error(err);
