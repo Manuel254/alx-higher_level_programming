d275a6aa8ac62c3a9c49c2742dd65b2c87e2fdfb
Manuel254 <immanuelkituku@gmail.com>
2023-04-25 10:29:45 +0000

    Fix semistandard error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 const f = require('fs');
 const filename = process.argv[2];
 
-f.readFile(filename, 'utf-8', function(err, data) {
-	if (err) throw err;
-	console.log(data);
+f.readFile(filename, 'utf-8', function (err, data) {
+  if (err) console.error(err);
+  else console.log(data);
 });
